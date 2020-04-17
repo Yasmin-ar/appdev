6cbaefc89189963d3a801fcb3a4379560e66b26e
1800924 <e1800924@shell.vamk.fi>
2020-04-17 20:08:02 +0300

    sound module added

--- testscreen.c
+++ testscreen.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "screen.h"
 #include "comm.h"
+#include "sound.h"
 
 int main(void){
 	Position cur = getscreensize();   // get screensize
@@ -33,6 +34,11 @@ int main(void){
 	clearscreen();
 	printf("color is set back to default\n");
 	getchar();
+	FILE *fp = fopen("test.wav", "r"); // open the file in read-only
+	WAVheader h = readwavhdr(fp);
+	fclose(fp);
+	displaywavhdr(h);
+
 /*
 	setfgcolor(CYAN);
 	gotoXY(14, 35);
